b34c6229464b82ed42e68424f81b19b89d5a86ae
FerryKurniawn <ferrysavvy@gmail.com>
2024-08-18 14:05:25 +0700

    fix the burger menu

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const burgerButton = document.getElementById("navbar-button");
 const burger = document.getElementById("navbar-default");
+const navList = document.getElementsByClassName("nav-menu");
 burgerButton.addEventListener("click", function () {
   burger.classList.toggle("hidden");
+});
+Array.from(navList).forEach((navItem) => {
+  navItem.addEventListener("click", function () {
+    burger.classList.add("hidden");
+  });
 });
